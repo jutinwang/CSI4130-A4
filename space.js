0be6fad969a403f06265cb096b970fb685e5fea9
justinwang <justinwang407@gmail.com>
2025-03-26 12:41:45 -0400

    Initial commit

---
+++ space.js
@@ -0,0 +1,5 @@
+import * as THREE from "three";
+import WebGL from 'three/addons/capabilities/WebGL.js';
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js';
+import { GUI } from "dat.gui";
+
